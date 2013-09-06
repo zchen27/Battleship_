22fbbef74bf896e6b9e2a68632cf305febca3db0
zchen0704 <zchen0704@CHHB45C4278BB2.inst.hcpss.org>
2013-09-06 14:08:37 -0400

    Changes test

---
+++ src/BoatTester.java
@@ -0,0 +1,43 @@
+
+public class BoatTester
+{
+    private Boat aircraftCarrier;
+    private Boat battleship;
+    private Boat cruiser;
+    private Boat submarine;
+    private Boat destroyer;
+    private Boat horizontalBoat;
+    private Boat verticalBoat;
+    private Boat topBoat;
+    private Boat bottomBoat;
+    private Boat leftBoat;
+    private Boat rightBoat;
+    private Boat centreBoat;
+    
+    public static void main(String[] args)
+    {
+        
+    }
+    
+    public static void testBoat()
+    {
+        
+    }
+    
+    public static void testName()
+    {
+        
+    }
+    
+    public static void testAbbreviation()
+    {
+        
+    }
+    
+    public static void testSize()
+    {
+        
+    }
+    
+    
+}
