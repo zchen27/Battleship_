64a536e84dddba501b83664d379dc99eda1c89aa
zchen0704 <zchen0704@CHHB45C4278BB2.inst.hcpss.org>
2013-09-06 13:54:31 -0400

    Changes test

--- src/Boat.java
+++ src/Boat.java
@@ -4,6 +4,7 @@ public class Boat
 	private String type;
 	private String direction;
 	private Position[] positions;
+    private boolean[] hits;
 	
 	public Boat(String t, Position p, String dir)
 	{
@@ -28,6 +29,11 @@ public class Boat
 		
 		positions = new Position[length];
 		positions[0] = start;
+        hits = new boolean[length];
+        for(int i = 0; i < length; i++)
+        {
+            hits[i] = false;
+        }
 		
 		for(int i = 1; i < length; i++)
 		{
@@ -56,13 +62,60 @@ public class Boat
 		char[] typeArray = type.toCharArray();
 		return typeArray[0];
 	}
+    
+    public String direction()
+    {
+        return direction;
+    }
+    
+    public int size()
+    {
+        return positions.length;
+    }
 	
 	public boolean onBoat(Position test)
 	{
-		for(int i = 0; i < length; i++)   
+        if(indexOf(test) != -1)
         {
-            
+            return true;
+        }
+        else
+        {
+            return false;
         }
 	}
+    
+    public void hit(Position test)
+    {
+       int i = indexOf(test);
+       if(i > -1 && i < positions.length)
+       {
+           hits[i] = true;
+       }
+    }
+    
+    private int indexOf(Position test)
+    {
+        for(int i = 0; i < positions.length; i++)   
+        {
+            if(positions[i].equals(test))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    public boolean sunk()
+    {
+        for(int i = 0; i < positions.length; i++)
+        {
+            if(!hits[i])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 	
 }
