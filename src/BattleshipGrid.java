05f5465fd0441407bf41a820c9f9ae126be6f95b
zchen27 <zchen27@gmail.com>
2013-09-22 22:10:22 -0400

    Semi-complete changes

---
+++ src/BattleshipGrid.java
@@ -0,0 +1,60 @@
+public class BattleshipGrid
+{
+    
+    private char[][] grid = new char[10][10];
+    
+    public BattleshipGrid()
+    {
+	for (int i = 0; i < grid.length; i++)
+	{
+	    for (int j = 0; j < grid.length; i++)
+	    {
+		grid[i][j] = '.';
+	    }
+	}
+    }
+    
+    public void shotAt(Position pos, boolean hit, char initial)
+    {
+	int col = pos.columnIndex();
+	int row = pos.rowIndex();
+	if (hit)
+	{
+	    grid[col][row] = initial;
+	}
+    }
+    
+    public boolean hit(Position pos)
+    {
+	int col = pos.columnIndex();
+	int row = pos.rowIndex();
+	if (grid[col][row] == '.' || grid[col][row] == '*')
+	{
+	    return false;
+	}
+	return true;
+    }
+    
+    public boolean miss(Position pos)
+    {
+	int col = pos.columnIndex();
+	int row = pos.rowIndex();
+	if (grid[col][row] == '*')
+	{
+	    return false;
+	}
+	return true;
+    }
+    
+    public boolean empty(Position pos)
+    {
+	int col = pos.columnIndex();
+	int row = pos.rowIndex();
+	if (grid[col][row] == '*')
+	{
+	    return false;
+	}
+	return true;
+    }
+    
+}
