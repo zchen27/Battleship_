de0c7cff6db2f51534c8da4b4962398a01a60000
zchen0704 <zchen0704@CHHB45C4278BB2.inst.hcpss.org>
2013-09-24 14:08:00 -0400

    Updated Ocean

--- src/Ocean.java
+++ src/Ocean.java
@@ -2,179 +2,88 @@
 public class Ocean
 {
 	private Boat[] boats = new Boat[5];
+	private Position[] occupied = new Position[17];
+	private Position[] hit = new Position[100];
+	private int placedBoats = 0;
+	private int filledPositions = 0;
+	
+	public Ocean()
+	{
+
+	}
 	
 	public void placeBoat(String boatName, String direction, Position pos) throws Exception
 	{
-		int i = 0;
-		while (boats[i] != null)
-		{
-			System.out.println(boats[i]); 
-			i++;
-		}
 		if(!checkInGrid(boatName, direction, pos))
 		{
 			throw new Exception("NOT TOTALLY IN GRID");
 		}
-		if(searchOverlap(boatName, direction, pos))
+		if(!searchCleared(boatName, direction, pos))
 		{
 			throw new Exception("OVERLAPPING ONE OR MORE BOATS");
 		}
-		
-		boats[i] = new Boat(boatName, pos, direction);
+		try
+		{
+			dropBoat(boatName, direction, pos);
+		}
+		catch (Exception e)
+		{
+			throw new Exception("MORE THAN FIVE BOATS");
+		}
 	}
 	
-	private boolean checkInGrid(String boatname, String direction, Position pos)
+	private void dropBoat(String boatName, String direction, Position pos)
 	{
-		int length = convertLength(boatname);
+		Boat boat = new Boat(boatName, pos, direction);
+		char abrv = boat.abbreviation();
 		int col = pos.columnIndex();
 		int row = pos.rowIndex();
 		
-		if(col < 0 || col > 9 || row < 0 || row > 9)
+		boats[placedBoats] = boat;
+		placedBoats++;
+		for(int i = 0; i < boat.size(); i++)
+		{
+			switch(direction)
+			{
+				case "HORIZONTAL":
+					occupied[filledPositions] = new Position(col + i, row);
+				case "VERTICAL":
+					occupied[filledPositions] = new Position(col, row + i);
+			}
+			filledPositions++;
+		}
+	}
+	
+	private boolean checkInGrid(String boatName, String direction, Position pos)
+	{
+		int col = pos.columnIndex();
+		int row = pos.rowIndex();
+		int size = new Boat(boatName, pos, direction).size();
+		if(col < 0 || row < 0 || col + size > 9 || row + size > 9)
 		{
 			return false;
 		}
 		
-		switch(direction)
+		return true;
+	}
+	
+	private boolean searchCleared(String boatName, String direction, Position pos)
+	{
+		Boat tempBoat = new Boat(boatName, pos, direction);
+		for(int i = 0; i <= filledPositions; i++)
 		{
-			case "HORIZONTAL":
-				if(col + length > 9)
-				{
-					return false;
-				}
-				break;
-			case "VERTICAL":
-				if(row + length > 9)
-				{
-					return false;
-				}
-				break;
+			if(tempBoat.onBoat(occupied[i]))
+			{
+				return false;
+			}	
 		}
 		return true;
 	}
 	
-	private boolean searchOverlap(String boatname, String direction, Position pos)
-	{
-		int length = convertLength(boatname);
-		int col = pos.columnIndex();
-		int row = pos.rowIndex();
-		for(int i = 0; i < length; i++)
-		{
-			if (checkOverlap(new Position(col, row)))
-			{
-				return true;
-			}
-			switch(direction)
-			{	
-				case "HORIZONTAL":
-					col++;
-					break;
-				case "VERTICAL":
-					row++;
-					break;		
-			}
-		}
-		return false;
-	}
-	
-	private boolean checkOverlap(Position pos)
-	{
-		for(int i = 0; i < boats.length; i++)
-		{
-			if(boats[i].onBoat(pos))
-			{
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	private int convertLength(String boatname)
-	{
-		switch (boatname)
-		{
-			case "Aircraft Carrier":
-				return 5;
-			case "Battleship":
-				return 4;
-			case "Cruiser":
-				return 3;
-			case "Submarine":
-				return 3;
-			case "Destroyer":
-				return 2;
-			default:
-				return -1;
-		}
-	}
-	
 	public void shootAt(Position pos)
 	{
-		for (int i = 0; i < boats.length; i++)
-		{
-			if(boats[i].onBoat(pos))
-			{
-			    boats[i].hit(pos);
-			}
-		}
-	}
-	
-	public boolean hit(Position pos)
-	{
-	    for(int i = 0; i < boats.length; i++)
-	    {
-			if(boats[i].isHit(pos))
-			{
-				return true;
-			}
-	    }
-	    return false;
-	}
-	
-	public boolean sunk(Position pos)
-	{
-	    for (int i = 0; i < boats.length; i++)
-	    {
-			if(boats[i].onBoat(pos) && boats[i].sunk())
-			{
-				return true;
-			}
-	    }
-	    return false;
-	}
-	
-	public boolean allSunk()
-	{
-	    for(int i = 0; i < boats.length; i++)
-	    {
-			if(!boats[i].sunk())
-			{
-				return false;
-			}
-	    }
-	    return true;
-	}
-	
-	public String boatName(Position pos)
-	{
-	    for (int i = 0; i < boats.length; i++)
-	    {
-			if(boats[i].onBoat(pos))
-			{
-				return boats[i].name();
-			}
-	    }
-	    return null;
-	}
-	
-	public char boatInitial(Position pos)
-	{
-	    for(int i = 0; i < boats.length; i++)
-	    {
-			if(boats[i].onBoat(pos))
-			{
-				return boats[i].abbreviation();
-			}
-	    }
-	    return '\u0000';
+		int col = pos.columnIndex();
+		int row = pos.rowIndex();
+		
 	}
 }
