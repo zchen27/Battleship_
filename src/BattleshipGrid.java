673dacfd80bb946efc484a11e2690b0e565430cc
zchen0704 <zchen0704@CHHB45C4278BB2.inst.hcpss.org>
2013-09-26 13:35:23 -0400

    Updated Grid

--- src/BattleshipGrid.java
+++ src/BattleshipGrid.java
@@ -1,60 +1,63 @@
 public class BattleshipGrid
 {
-    
-    private char[][] grid = new char[10][10];
-    
-    public BattleshipGrid()
-    {
-	for (int i = 0; i < grid.length; i++)
+	private char[][] grid = new char[10][10];
+
+	public BattleshipGrid()
 	{
-	    for (int j = 0; j < grid.length; i++)
-	    {
-		grid[i][j] = '.';
-	    }
+		for (int i = 0; i < grid.length; i++)
+		{
+			for (int j = 0; j < grid.length; i++)
+			{
+				grid[i][j] = '.';
+			}
+		}
 	}
+    
+	public void shotAt(Position pos, boolean hit, char initial)
+	{
+		int col = pos.columnIndex();
+		int row = pos.rowIndex();
+		if (hit)
+		{
+			grid[col][row] = initial;
+		}
+		else
+		{
+			grid[col][row] = '*';
+		}
     }
     
-    public void shotAt(Position pos, boolean hit, char initial)
-    {
-	int col = pos.columnIndex();
-	int row = pos.rowIndex();
-	if (hit)
+	public boolean hit(Position pos)
 	{
-	    grid[col][row] = initial;
+		int col = pos.columnIndex();
+		int row = pos.rowIndex();
+		if (grid[col][row] == '.' || grid[col][row] == '*')
+		{
+			return false;
+		}
+		return true;
 	}
-    }
     
-    public boolean hit(Position pos)
-    {
-	int col = pos.columnIndex();
-	int row = pos.rowIndex();
-	if (grid[col][row] == '.' || grid[col][row] == '*')
+	public boolean miss(Position pos)
 	{
-	    return false;
-	}
-	return true;
-    }
-    
-    public boolean miss(Position pos)
-    {
-	int col = pos.columnIndex();
-	int row = pos.rowIndex();
-	if (grid[col][row] == '*')
-	{
-	    return false;
-	}
-	return true;
+		int col = pos.columnIndex();
+		int row = pos.rowIndex();
+		if (grid[col][row] == '*')
+		{
+			return false;
+		}
+		return true;
     }
     
     public boolean empty(Position pos)
-    {
-	int col = pos.columnIndex();
-	int row = pos.rowIndex();
-	if (grid[col][row] == '*')
 	{
-	    return false;
-	}
-	return true;
+		int col = pos.columnIndex();
+		int row = pos.rowIndex();
+		if (grid[col][row] == '.')
+		{
+			return false;
+		}
+		return true;
     }
     
 }
