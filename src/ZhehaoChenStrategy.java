bd57c1a1717754236d527b550343cba114f4bdad
zchen27 <zchen27@gmail.com>
2013-10-17 23:35:13 -0400

    Commit

--- src/ZhehaoChenStrategy.java
+++ src/ZhehaoChenStrategy.java
@@ -13,13 +13,13 @@ public class ZhehaoChenStrategy extends ComputerBattleshipPlayer
 		TWO,
 		THREE,
 		FOUR,
-		FIVE;
+		FIVE
 	}
 	
 	private Status status;
 	private Parity parity;
 	private int maxHoleSize;
-	private ArrayList<Position[]> holes;
+	private ArrayList<Position> candidates;
 	private boolean CASunk = false;
 	private boolean BBSunk = false;
 	private boolean CCSunk = false;
@@ -30,23 +30,24 @@ public class ZhehaoChenStrategy extends ComputerBattleshipPlayer
 	{
 		status = Status.HUNT;
 		parity = Parity.TWO;
-		holes = new ArrayList();
+		candidates = new ArrayList();
 	}
 	
-	private int convertHoleSize(Parity parity)
+	private void generateCandidates()
 	{
-		switch (parity)
+		for(int i = 0; i < 10; i++)
 		{
-			case TWO:
-				return 1;
-			case THREE:
-				return 2;
-			case FOUR:
-				return 3;
-			case FIVE:
-				return 4;
-			default:
-				return -1;
+			for(int j = 0; j < 5; j++)
+			{
+				if(i % 2 != 0)
+				{
+					candidates.add(new Position(i, (2 * j + 1)));
+				}
+				else
+				{
+					candidates.add(new Position(i, (2 * j)));
+				}
+			}
 		}
 	}
 	
@@ -54,4 +55,5 @@ public class ZhehaoChenStrategy extends ComputerBattleshipPlayer
 	{
 		return null;
 	}
+	
 }
